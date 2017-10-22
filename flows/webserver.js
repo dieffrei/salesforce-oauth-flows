693e5d6d6181bf0b8021e8e2108e0ebf74ec869d
Diéffrei Tiepo de Quadros <dieffrei@gmail.com>
2017-10-22 20:09:43 -0200

    webserver flow sample

---
+++ flows/webserver.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const settings = require("../settings.json");
+
+app.get('/login', function (req, res) {
+    res.redirect('https://login.salesforce.com/services/oauth2/authorize?' 
+        + '&response_type=code'
+        + '&client_id=' + settings['consumer-key']
+        + '&redirect_uri=' + settings['redirect-uri']
+    );
+})
+
+app.get('/oauth2/callback', function (req, res) {
+    res.send(req.query.code);
+})
+    
+app.listen(3000, function () {
+  console.log('App listening on port 3000!')
+})
